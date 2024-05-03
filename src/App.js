2290989ee23ce3e25ef41ba8c8b81c74910b7919
gcolley <geremy.colley@cegedim.com>
2024-05-03 16:33:11 +0200

    add css

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ function App() {
           </header>
           <article className="article-main" style={{backgroundImage: `url(${dungeonImage})`}}>
             <div className="m-article-content">
-              <h2 className='m-header'>Story to be continued</h2>
-              <p className='m-paragraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum tenetur culpa atque fugiat? Neque obcaecati sunt, porro suscipit quos nesciunt a dignissimos consectetur consequatur? Ducimus accusamus veniam voluptates libero dolore?</p>
+              <h2 className="m-header">Story to be continued</h2>
+              <p className="m-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
           </article>
         </div>
