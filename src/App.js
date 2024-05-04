283e239fe2a71f53c45fa4b65560144ec9d551b2
gcolley <geremy.colley@cegedim.com>
2024-05-04 10:33:33 +0200

    Correction de bug

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
 import layerBaseImage from './img/layer-base.png';
-import layerMidImage from './img/layer-middle.png';
+import layerMiddleImage from './img/layer-middle.png';
 import layerFrontImage from './img/layer-front.png';
 import dungeonImage from './img/dungeon.jpg';
+import './index.css';
 
 function App() {
 
@@ -24,19 +25,16 @@ function App() {
                   <div className="title">Dark forest</div>
                 </div>
                 <div className="img-layer layer-base" style={{ backgroundImage : `url(${layerBaseImage})`}}></div>
-                <div className="img-layer layer-mid" style={{ backgroundImage : `url(${layerMidImage})`}}></div>
+                <div className="img-layer layer-mid" style={{ backgroundImage : `url(${layerMiddleImage})`}}></div>
                 <div className="img-layer layer-front" style={{ backgroundImage : `url(${layerFrontImage})`}}></div>
-
               </div>
           </header>
           <article className="article-main" style={{backgroundImage: `url(${dungeonImage})`}}>
             <div className="m-article-content">
               <h2 className="m-header">Story to be continued</h2>
-              <p className="m-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p className="m-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.velit esse cillum dolore eu fugiat nulla pariatur.</p>
             </div>
-            <div className="copy">
-              Gérémy
-            </div>
+            <div className="copy">Gérémy</div>
           </article>
         </div>
       </div>
@@ -45,3 +43,10 @@ function App() {
 }
 
 export default App;
+
+
+/* 
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+19,20
+*/
